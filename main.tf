6e7d39ec97c25646b0eb9f224d830933a5b8d9a2
n3xus75 <artur.kolodz@gmail.com>
2025-10-01 13:21:45 +0000

    piec

--- main.tf
+++ main.tf
@@ -27,11 +27,16 @@ resource "azurerm_service_plan" "example" {
   sku_name            = "P0v3"
 }
 
-
 resource "azurerm_linux_web_app" "example" {
   name                = "example-webapp-tutajnazwa333"
   location            = "westeurope"
   resource_group_name = "rg-03"
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
+}
+
+resource "azurerm_storage_container" "gallery" {
+  name                  = "gallery"
+  storage_account_name = "warsztatowe3245"
+  container_access_type = "blob"
 }
