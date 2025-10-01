48cdd90f4698e1f6b63a42b294c8c78c350711ad
n3xus75 <artur.kolodz@gmail.com>
2025-10-01 13:29:11 +0000

    szesc

--- main.tf
+++ main.tf
@@ -37,6 +37,14 @@ resource "azurerm_linux_web_app" "example" {
 
 resource "azurerm_storage_container" "gallery" {
   name                  = "gallery"
-  storage_account_name = "warsztatowe3245"
+  storage_account_name = azurerm_storage_account.gallery_storage.name
   container_access_type = "blob"
+}
+
+resource "azurerm_storage_account" "gallery_storage" {
+  name                     = "gallery33737ss"
+  resource_group_name      = "rg-03"
+  location                 = "westeurope"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
